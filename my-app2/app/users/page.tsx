ca3f310fe5e1acac8e3d0512acf8e03181899050
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-07 22:45:07 +0530

    Server Side Rendering using NextJS

--- my-app2/app/users/page.tsx
+++ my-app2/app/users/page.tsx
@@ -1,29 +1,15 @@
-"use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 interface UserData {
   name: string;
   email: string;
 }
 
-export default function User() {
-  const [data, setData] = useState<UserData | undefined>();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    axios
-      .get<UserData>(
-        "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details",
-      )
-      .then((response) => {
-        setData(response.data);
-        setLoading(false);
-      });
-  }, []);
-  if (loading || !data) {
-    return <div>Loading......</div>;
-  }
+export default async function User() {
+  const response = await axios.get<UserData>(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details",
+  );
+  const data = response.data;
   return (
     <div>
       User Page
