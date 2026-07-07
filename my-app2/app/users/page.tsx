b049e34d98b048aedde1ec8b679eb43e8f1a4751
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-07 23:40:36 +0530

    frontend and backend in the same repo

--- my-app2/app/users/page.tsx
+++ my-app2/app/users/page.tsx
@@ -7,7 +7,7 @@ interface UserData {
 
 export default async function User() {
   const response = await axios.get<UserData>(
-    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details",
+    "http://localhost:3000/api/v1/user/details",
   );
   const data = response.data;
   return (
