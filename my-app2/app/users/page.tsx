03ea70fd4ce14f2abc5d968e05fbe099afaf47fc
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-07 22:13:43 +0530

    NextJS frontend calling backend (No nextJS backend)

---
+++ my-app2/app/users/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+export default function User() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details",
+      )
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      });
+  }, []);
+  if (loading) {
+    return <div>Loading......</div>;
+  }
+  return (
+    <div>
+      User Page
+      {data.name}
+      {data.email}
+    </div>
+  );
+}
